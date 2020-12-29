d7bd7ed6bf9d1879101a769512b682a117d7fa2d
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2020-12-29 21:54:16 +0000

    Create 1 - If odd or even.py

---
+++ PE/PE 01/1 - If odd or even.py
@@ -0,0 +1,13 @@
+# Created on November, 2020
+# @author: Fábio Araújo de Sá
+
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+
+if n1%2 == 1:
+    result = n2*n3
+else:
+    result = n2 - n3
+
+print(result)
