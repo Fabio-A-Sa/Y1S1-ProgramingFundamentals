2bfac0b6e2f7e9e190f1c9fa55bc2cc9717f9ea0
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2020-12-29 21:59:53 +0000

    Create 4 - Multiplicative persistence.py

---
+++ PE/PE 01/4 - Multiplicative persistence.py
@@ -0,0 +1,11 @@
+# Code by JCL
+
+num = int(input("num? "))
+count = 0
+while num > 9:    
+    aux = 1    
+    while num != 0:        
+        aux = (num % 10) * aux        
+        num = num // 10    
+        num = aux    
+count += 1print(count)
