fd3da9212f4ae24c9fc9133acdb45bb9bbc1daed
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-04 17:09:33 +0000

    Create 4 - Generate multiples of 7.py

---
+++ RE/RE 10 - Effect-free programming/4 - Generate multiples of 7.py
@@ -0,0 +1,14 @@
+# Created on January, 2021
+# @author: Fábio Araújo de Sá
+
+def multiples_of7(n):
+
+    if n < 0:
+       return None
+
+    # All multiples of 7 in nterval [0, n[ (n not include)
+    all_possibilities = [x for x in range(0, n) if x%7 == 0]
+
+    # Generator
+    for item in all_possibilities:
+        yield item
