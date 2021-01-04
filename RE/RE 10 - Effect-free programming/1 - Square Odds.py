f69adc88e56ea3a7446d1e62e24abcf4cb9d3c34
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-04 17:04:15 +0000

    Create 1 - Square Odds.py

---
+++ RE/RE 10 - Effect-free programming/1 - Square Odds.py
@@ -0,0 +1,6 @@
+def square_odds(values):
+
+    all_numbers = [int(x) for x in values.split(",")]
+    all_squares = [str(x**2) for x in all_numbers if x%2==1]
+    result = ",".join(all_squares)
+    return result
