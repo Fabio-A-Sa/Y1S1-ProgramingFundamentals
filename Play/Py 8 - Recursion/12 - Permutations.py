62afea3825966ca4859eb8b63bfa197751363169
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-07 16:19:36 +0000

    Update 12 - Permutations.py

--- Play/Py 8 - Recursion/12 - Permutations.py
+++ Play/Py 8 - Recursion/12 - Permutations.py
@@ -1,2 +1,27 @@
 # Created on December, 2020
 # @author: Fábio Araújo de Sá
+
+def permuta(alist, step=0):
+
+    all_permutations = []
+
+    if len(alist) == 1:
+        # Base case
+        return list([alist])
+    
+    if len(alist) == 2:
+        return [alist, [alist[1], alist[0]]]
+
+    if len(alist) == step:
+        all_permutations.append(alist)
+
+    if len(alist) > 2:
+        a = step
+        b = len(alist)
+        for item in range(a, b):
+            cop = alist.copy()
+            cop[item] = alist[a]
+            cop[a] = alist[item]
+            all_permutations += permuta(cop, 1+a)
+        
+        return all_permutations
