84af45c679c3aa20611b16b5fe5724344b6d51dd
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-07 16:19:54 +0000

    Update 11 - Painter's problem.py

--- Play/Py 8 - Recursion/11 - Painter's problem.py
+++ Play/Py 8 - Recursion/11 - Painter's problem.py
@@ -1,2 +1,20 @@
 # Created on December, 2020
 # @author: Fábio Araújo de Sá
+
+def poss(n, boards):
+
+    # All possibilities to paint all boards
+    qtd = len(boards) + 2 - n
+    all_possibilities = []
+    for x in range(1, qtd):
+        possibility = paint(n-1, boards[x:]) + max(boards[:x])
+        all_possibilities.append(possibility)
+    
+    return all_possibilities
+
+def paint(n, boards):
+
+    if n == 1:
+        return max(boards)
+    else:
+        return min(poss(n, boards))
