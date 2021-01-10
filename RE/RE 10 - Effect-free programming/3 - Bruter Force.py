1af6771e43d781ed5f0499215bd2ace48f4523f9
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-10 15:10:19 +0000

    Update 3 - Bruter Force.py

--- RE/RE 10 - Effect-free programming/3 - Bruter Force.py
+++ RE/RE 10 - Effect-free programming/3 - Bruter Force.py
@@ -5,7 +5,7 @@ def brute_force(f, l):
 
     # All combinations whose lenght is 3 and not (x == y or x == z or z == y)
     all_combinations = list([(x, y, z) for x in l for y in l for z in l])
-    concatenated= list(["".join(possibility) for possibility in all_combinations])
+    concatenated = list(["".join(possibility) for possibility in all_combinations])
 
     # Filter with the function f
     result = list([possibility for possibility in concatenated if f(possibility) == True])
