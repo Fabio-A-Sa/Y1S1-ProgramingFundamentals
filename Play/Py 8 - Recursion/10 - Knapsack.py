8c54359def58019473dbf8e59a70a03a3b53dae0
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-10 22:54:11 +0000

    Update 10 - Knapsack.py

--- Play/Py 8 - Recursion/10 - Knapsack.py
+++ Play/Py 8 - Recursion/10 - Knapsack.py
@@ -1,56 +1,68 @@
-# Created on December, 2020
-# @author: Fábio Araújo de Sá
+def qual_o_preço_desta_montra_final_haaaaaa(money, products, wishlist):
 
-def não_falido(money, products, wishlist):
+    total = 0 
+    for coisa in wishlist:
+        quantidade = wishlist[coisa]
+        preço = products[coisa]
+        total = total + quantidade * preço
+        
+    return tuple((total, False)) if total > money else tuple((total, True))
 
-    gasto = 0
-    for item in wishlist:
-        quantidade = wishlist.get(item)
-        preço = products.get(item)
-        gasto = gasto + preço*quantidade
+def normalization(alist):
 
-    return True if (gasto < money or money == gasto) else False
+    standardized = []
+
+    for adict in alist:
+        dictio = adict.copy()
+        for key in dictio:
+            if adict[key] == 0:
+                del dictio[key]
+        standardized.append(dictio)
+
+    return standardized
 
 def knapsack(money, products, wishlist):
 
     all_possibilities = []
+    best_combination = 0
+    best_solution = {}
 
-    if não_falido(money, products, wishlist):
-        # Found the best solution
-        return wishlist
+    if (qual_o_preço_desta_montra_final_haaaaaa(money, products, wishlist))[1]:
+        return wishlist # Found one possible solution
     
     # Rearranging
-    for item in wishlist:
-        solution = wishlist.copy()
-        if wishlist.get(item) != 0:
-            if products.get(item) > money:
-                # Can't buy them
-                solution[item] = 0
-            else:
-                # Can't buy them all
-                solution[item] = solution[item] - 1
-            
-            all_possibilities.append(solution)
+    else:
+        for item in wishlist:
+            solution = wishlist.copy()
 
-    for possibility in all_possibilities:
-        if não_falido(money, products, possibility):
-            continue
-        else:
-            # Error
-            for item in possibility:
-                nova = knapsack(money, products, item)
-                all_possibilities.append(nova)
-    
-    all_prices = []
-    for possibility in all_possibilities:
-        quantidade = wishlist.get(possibility)
-        preço = products.get(possibility)
-        gasto = gasto + preço*quantidade
-        all_prices.append(gasto)
+            if wishlist.get(item) != 0:
 
-    final = {}
-    for item, qtd in enumerate(all_possibilities.index(max(all_prices)).itens()):
-        if qtd != 0:
-            final = final + {item:qtd}
+                if solution[item] == 1:
+                    del solution[item] # solution[item] - 1 == 0
 
-    return final
+                elif products.get(item) > money:
+                    del solution[item] # Can't buy them
+
+                else:
+                    solution[item] = solution[item] - 1 # Can't buy them all
+                
+                possibility = qual_o_preço_desta_montra_final_haaaaaa(money, products, solution)
+
+                if possibility[1] == True:
+                    if possibility[0] > best_combination:
+
+                        best_combination = possibility[0]
+                        best_solution = solution
+                        all_possibilities.append(best_solution)
+                else:
+                    other_possibility = knapsack(money, products, solution)
+                    if type(other_possibility) is dict:
+                        other_sum = qual_o_preço_desta_montra_final_haaaaaa(money, products, other_possibility)
+                        if other_sum[1] == True:
+                            if other_sum[0] > best_combination:
+
+                                best_combination = other_sum[0]
+                                best_solution = other_possibility
+                                all_possibilities.append(best_solution)
+
+    return (normalization(all_possibilities))[-1]
