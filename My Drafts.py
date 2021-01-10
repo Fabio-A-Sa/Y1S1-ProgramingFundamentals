d707ac06e1bea37329adf4f59890c78167967329
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-10 22:19:19 +0000

    Update My Drafts.py

--- My Drafts.py
+++ My Drafts.py
@@ -7204,3 +7204,81 @@ def lecture_math(something):
     # Now I can make counts without "math" before assignment!
     a = sin(0.585)
     return a
+
+      def qual_o_preço_desta_montra_final_haaaaaa(money, products, wishlist):
+
+    total = 0 
+    for coisa in wishlist:
+        quantidade = wishlist[coisa]
+        preço = products[coisa]
+        total = total + quantidade * preço
+        
+    return tuple((total, False)) if total > money else tuple((total, True))
+
+def normalization(alist):
+
+    standardized = []
+
+    for adict in alist:
+        dictio = adict.copy()
+        for key in dictio:
+            if adict[key] == 0:
+                del dictio[key]
+        standardized.append(dictio)
+
+    return standardized
+
+def knapsack(money, products, wishlist):
+
+    all_possibilities = []
+    best_combination = 0
+    best_solution = {}
+
+    if (qual_o_preço_desta_montra_final_haaaaaa(money, products, wishlist))[1]:
+        return wishlist # Found one possible solution
+    
+    # Rearranging
+    else:
+        for item in wishlist:
+
+            solution = wishlist.copy()
+
+            if wishlist.get(item) != 0:
+
+                if solution[item] == 1:
+                    # solution[item] - 1 == 0
+                    del solution[item]
+
+                elif products.get(item) > money:
+                    # Can't buy them
+                    del solution[item]
+
+                else:
+                    # Can't buy them all
+                    solution[item] = solution[item] - 1
+                
+                possibility = qual_o_preço_desta_montra_final_haaaaaa(money, products, solution)
+
+                if possibility[1] == True:
+                    if possibility[0] > best_combination:
+
+                        best_combination = possibility[0]
+                        best_solution = solution
+                        all_possibilities.append(best_solution)
+                
+                else:
+
+                    other_possibility = knapsack(money, products, solution)
+
+                    if type(other_possibility) is dict:
+
+                        other_sum = qual_o_preço_desta_montra_final_haaaaaa(money, products, other_possibility)
+
+                        if other_sum[1] == True:
+                            if other_sum[0] > best_combination:
+                                
+                                best_combination = other_sum[0]
+                                best_solution = other_possibility
+                                all_possibilities.append(best_solution)
+
+    return (normalization(all_possibilities))[-1]
