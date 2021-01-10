440ed2b4c2eea59cc41f524414a2c55a7bf86c6a
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-10 15:09:12 +0000

    Update 1 - Square Odds.py

--- RE/RE 10 - Effect-free programming/1 - Square Odds.py
+++ RE/RE 10 - Effect-free programming/1 - Square Odds.py
@@ -3,7 +3,18 @@
 
 def square_odds(values):
 
-    all_numbers = [int(x) for x in values.split(",")]
-    all_squares = [str(x**2) for x in all_numbers if x%2==1]
-    result = ",".join(all_squares)
-    return result
+    if values == ("" or ","):
+        return ""
+
+    if type(values) != str:
+        return ""
+
+    if values == "0":
+        return ""
+
+    else:
+
+        all_numbers = [int(x) for x in values.split(",")]
+        all_squares = [str(x**2) for x in all_numbers if x%2==1]
+        result = ",".join(all_squares)
+        return result
