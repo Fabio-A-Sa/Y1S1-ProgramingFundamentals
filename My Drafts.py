ff0ddf31af649aca2dd9a78bbcb8b30dde0f83e7
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-11 15:13:00 +0000

    Update My Drafts.py

--- My Drafts.py
+++ My Drafts.py
@@ -7331,3 +7331,68 @@ def multiples_of7(n):
         if x%7 == 0:
             # Generator
             yield x
+
+      def aux(f):
+    return f if type(f) is list else f()
+
+def bitonic_point(f):
+
+    alist = aux(f)
+
+    if len(alist)%2 == 1:
+        middle = (len(alist)+1)//2
+    else:
+        middle = len(alist)//2
+
+    left = alist[:middle]
+    right = alist[middle:]
+
+    if alist[middle - 1] < alist[middle] and alist[middle] > alist[middle + 1]:
+        # Atingiu o pico
+        return alist[middle]
+
+    if alist[middle-1] < alist[middle]:
+        # O número procurado está depois
+        return bitonic_point(right)
+    
+    if alist[middle] > alist[middle + 1]:
+        # O número procurado está antes
+        return bitonic_point(left) 
+
+    # f = list(range(0, 10)) + list(range(20, 2, -1))
+    # print(bitonic_point(f))
+
+def aux(f):
+    return f if type(f) is list else f()
+
+def count_zeros(f):
+
+    alist = aux(f)
+    total = 0
+
+    if len(alist) == 1:
+        # Caso base
+        return total
+
+    else:
+            
+        if len(alist)%2 == 1:
+            middle = int((len(alist)+1)//2)
+        else:
+            middle = int(len(alist)//2)
+
+        left = alist[:middle]
+        right = alist[middle:]
+
+        if alist[middle] != 0:
+            # Os zeros estão mais à frente
+            total = total + count_zeros(right)
+
+        if alist[middle] == 0:
+            # Inclui tudo para a frente mais um pedaço de trás
+            total = total + len(right) + count_zeros(left)
+
+    return total
+
+    # f = lambda: [1, 1, 1, 0, 0]
+    # print(count_zeros(f))
