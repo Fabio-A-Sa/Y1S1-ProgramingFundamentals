b5ecd6fa717ae7acbb22845225bd7959c12f56df
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-18 23:31:07 +0000

    Update My Drafts.py

--- My Drafts.py
+++ My Drafts.py
@@ -7843,3 +7843,54 @@ def tic_tac_toe(board, player):
     i = posição + 1
 
     return "{}{}".format(l, i)
+      
+def combinations(alist):
+
+    combinations = list([(x, y) for x in alist for y in alist if x != y])
+    return combinations
+
+def is_coliding(atuple):
+
+    def A_B(atuple):
+
+        # A is to the left of B
+        ret_A = atuple[0]
+        ret_B = atuple[1]
+
+        first_condition = ret_A['x2'] > ret_B['x1'] and ret_A['x2'] < ret_B['x2']
+        second_condition = ret_A['y1'] > ret_B['y2'] and ret_A['y1'] < ret_B['y1']
+
+        third_condition = ret_A['x2'] > ret_B['x1'] and ret_A['x2'] < ret_B['x2']
+        fourth_condition = ret_A['y2'] > ret_B['y2'] and ret_A['y1'] < ret_B['y1']
+
+        return (first_condition and second_condition) or (third_condition and fourth_condition)
+
+    def B_A(atuple):
+
+        # B is to the left of A
+        ret_A = atuple[1]
+        ret_B = atuple[0]
+
+        first_condition = ret_A['x2'] > ret_B['x1'] and ret_A['x2'] < ret_B['x2']
+        second_condition = ret_A['y1'] > ret_B['y2'] and ret_A['y1'] < ret_B['y1']
+
+        third_condition = ret_A['x2'] > ret_B['x1'] and ret_A['x2'] < ret_B['x2']
+        fourth_condition = ret_A['y2'] > ret_B['y2'] and ret_A['y1'] < ret_B['y1']
+
+        return (first_condition and second_condition) or (third_condition and fourth_condition)
+
+    return A_B(atuple) or B_A(atuple)
+
+def number_of_collisions(objects):
+
+    prob_colisions = combinations(objects)
+
+    total = 0
+    for item in prob_colisions:
+
+        if is_coliding(item):
+            total = total + 1
+        else:
+            continue
+
+    return total
