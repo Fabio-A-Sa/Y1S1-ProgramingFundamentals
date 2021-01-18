6c7ce536b56b4ca909357c325d690f06c2e9a1b6
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-18 23:31:28 +0000

    Update 1 - Colisions.py

--- RE/RE 12 - Solving problems/1 - Colisions.py
+++ RE/RE 12 - Solving problems/1 - Colisions.py
@@ -11,11 +11,11 @@ def is_coliding(atuple):
         ret_A = atuple[0]
         ret_B = atuple[1]
 
-        first_condition = ret_A['x2'] in range (ret_B['x1'], ret_B['x2'])
-        second_condition = ret_A['y1'] in range (ret_B['y2'], ret_B['y1'])
+        first_condition = ret_A['x2'] > ret_B['x1'] and ret_A['x2'] < ret_B['x2']
+        second_condition = ret_A['y1'] > ret_B['y2'] and ret_A['y1'] < ret_B['y1']
 
-        third_condition = ret_A['x2'] in range (ret_B['x1'], ret_B['x2'])
-        fourth_condition = ret_A['y2'] in range (ret_B['y2'], ret_B['y1'])
+        third_condition = ret_A['x2'] > ret_B['x1'] and ret_A['x2'] < ret_B['x2']
+        fourth_condition = ret_A['y2'] > ret_B['y2'] and ret_A['y1'] < ret_B['y1']
 
         return (first_condition and second_condition) or (third_condition and fourth_condition)
 
@@ -25,11 +25,11 @@ def is_coliding(atuple):
         ret_A = atuple[1]
         ret_B = atuple[0]
 
-        first_condition = ret_A['x2'] in range (ret_B['x1'], ret_B['x2'])
-        second_condition = ret_A['y1'] in range (ret_B['y2'], ret_B['y1'])
+        first_condition = ret_A['x2'] > ret_B['x1'] and ret_A['x2'] < ret_B['x2']
+        second_condition = ret_A['y1'] > ret_B['y2'] and ret_A['y1'] < ret_B['y1']
 
-        third_condition = ret_A['x2'] in range (ret_B['x1'], ret_B['x2'])
-        fourth_condition = ret_A['y2'] in range (ret_B['y2'], ret_B['y1'])
+        third_condition = ret_A['x2'] > ret_B['x1'] and ret_A['x2'] < ret_B['x2']
+        fourth_condition = ret_A['y2'] > ret_B['y2'] and ret_A['y1'] < ret_B['y1']
 
         return (first_condition and second_condition) or (third_condition and fourth_condition)
 
