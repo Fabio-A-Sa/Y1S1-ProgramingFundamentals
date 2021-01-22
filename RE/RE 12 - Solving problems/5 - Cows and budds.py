06d6df93f280b949bf326737d7223e0159fb51a7
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-22 18:35:16 +0000

    Create 5 - Cows and budds.py

---
+++ RE/RE 12 - Solving problems/5 - Cows and budds.py
@@ -0,0 +1,26 @@
+import random
+
+def cows_bulls(seed):
+
+    # Generate a number with
+    rng = random.Random(seed)
+    number = rng.randrange(0000, 10000)
+    # inpu
+    def inp():
+        inp = int(("Something"))
+        return inp
+
+    inp()
+
+
+    # Comparation
+    cows = 0
+    bulls = 0
+
+
+
+    return cows, bulls
+seed = int(input())
+cows_bulls(seed)
+guess = int(imput())
+print(guess)
