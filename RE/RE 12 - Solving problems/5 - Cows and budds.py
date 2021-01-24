10eae31c0a3fdd1873e308bee0bbddb3e9134d94
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-24 16:00:25 +0000

    Update 5 - Cows and budds.py

--- RE/RE 12 - Solving problems/5 - Cows and budds.py
+++ RE/RE 12 - Solving problems/5 - Cows and budds.py
@@ -1,26 +1,39 @@
+# Created on January, 2021
+# @author: Fábio Araújo de Sá
+
+# Import module random
 import random
 
 def cows_bulls(seed):
+    
+    # Random seed from FPRO
+    random.seed(seed)
+    
+    def how_many(guess):
 
-    # Generate a number with
-    rng = random.Random(seed)
-    number = rng.randrange(0000, 10000)
-    # inpu
-    def inp():
-        inp = int(("Something"))
-        return inp
+        certo = random.randint(0000, 9999)
+        
+        correct = [x for x in str(certo)]
+        number = [x for x in str(guess)]
+        
+        # Cows - Correct number in correct position
+        cows = 0
+        for index, value in enumerate(number):
 
-    inp()
-
-
-    # Comparation
-    cows = 0
-    bulls = 0
-
-
-
-    return cows, bulls
-seed = int(input())
-cows_bulls(seed)
-guess = int(imput())
-print(guess)
+            if value == correct[index]:                
+                if index == correct.index(value):
+                        
+                        cows = cows + 1
+                        correct[index] = "x"
+                        
+        # Bulls - Correct number in wrong position
+        bulls = 0        
+        for index, value in enumerate(number):
+           
+            if value in correct:      
+                bulls = bulls + 1
+                correct[correct.index(value)] = "x"
+                    
+        return cows, bulls
+    
+    return how_many
