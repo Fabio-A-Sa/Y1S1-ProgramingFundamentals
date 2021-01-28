606551731d6468a2f69878869607fc24005b582d
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-28 21:13:09 +0000

    Update 8 - Nested exceptions.py

--- Play/Py 13 - Files and exceptions/8 - Nested exceptions.py
+++ Play/Py 13 - Files and exceptions/8 - Nested exceptions.py
@@ -1,2 +1,24 @@
 # Created on January, 2021
 # @author: Fábio Araújo de Sá
+
+def nested_exceptions(tree):
+
+    all_answers = []
+
+    for item in tree:
+
+        # Base case, is a function
+        if callable(item):
+            try:
+                item()
+                all_answers.append(False)
+            except:
+                all_answers.append(True)
+
+        else:
+            # Recursive search
+            something = nested_exceptions(item)
+            all_answers.append(something)
+
+    answers = tuple(all_answers)
+    return answers
