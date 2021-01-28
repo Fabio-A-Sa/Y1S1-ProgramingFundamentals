19632d783777a9d8d402816e8d846f95415ecb1a
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-28 15:42:27 +0000

    Update My Drafts.py

--- My Drafts.py
+++ My Drafts.py
@@ -8717,3 +8717,47 @@ def maximum_depth(alist):
 
     total = total + 1 # Add [] form        
     return total
+      
+def maximum_depth(alist):
+    
+    # Caso base
+    if alist == []:
+        return 1
+
+    # Else
+    total = 0
+    for item in alist:
+        sub_total = maximum_depth(item)
+        if sub_total > total:
+            total = sub_total
+
+    total = total + 1 # Add [] form        
+    return total
+
+# First solution
+
+# importing functools for reduce()
+import functools
+
+def nested_fmr(f, m, r, lst):
+    # using recursion to deal with the nested list
+    # base case
+    if type(lst) != list:
+        return lst
+    # iteration with the recursive call
+    aux = []
+    for l in lst:
+        aux.append(nested_fmr(f, m, r, l))
+    
+    return functools.reduce(r, map(m, filter(f, aux)))
+
+# Second solution
+
+def nested_fmr2(f, m, r, lst):
+    # solve it with the focus on data (as in PE04)  
+    if type(lst) != list:
+        return lst
+    aux = [nested_fmr2(f, m, r, l) for l in lst]
+
+    return functools.reduce(r, map(m, filter(f, aux)))
+
