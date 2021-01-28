da26257306b1503b8eecf60055763e830f695c38
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-28 21:13:35 +0000

    Update My Drafts.py

--- My Drafts.py
+++ My Drafts.py
@@ -9062,3 +9062,25 @@ def diff(filename1, filename2):
         result = result + item
     
     return result
+
+def nested_exceptions(tree):
+
+    all_answers = []
+
+    for item in tree:
+
+        # Base case, is a function
+        if callable(item):
+            try:
+                item()
+                all_answers.append(False)
+            except:
+                all_answers.append(True)
+
+        else:
+            # Recursive search
+            something = nested_exceptions(item)
+            all_answers.append(something)
+
+    answers = tuple(all_answers)
+    return answers
