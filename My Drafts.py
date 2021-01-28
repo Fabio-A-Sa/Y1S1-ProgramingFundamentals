4317db7f86642bf43087e8d3fc9d54e38c625785
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-28 14:10:35 +0000

    Update My Drafts.py

--- My Drafts.py
+++ My Drafts.py
@@ -8655,3 +8655,49 @@ def closest_pair(points):
     answer = min(all_values)
 
     return answer
+      
+ def search_map(amap, map_rectangle, search_rectangle):
+
+    # Condição Base
+    if amap is None:
+        return set()
+    
+    if type(amap) == str:
+        return set(amap)
+
+    # Definir os mapas
+    mapx = map_rectangle[0]
+    mapy = map_rectangle[1]
+    mapz = map_rectangle[2]
+    mapt = map_rectangle[3]
+
+    # Definir áreas procuradas
+    x = search_rectangle[0]
+    y = search_rectangle[1]
+    z = search_rectangle[2]
+    t = search_rectangle[3]
+
+    answer = set() # Acumulador dos objectos
+
+    if x <= mapx + mapz/2 and y <= mapy + mapt/2:
+        # Se estiver no quadrante 1, chamada recursiva
+        answer = answer | search_map(amap[0], (mapx, mapy, mapz/2, mapt/2), search_rectangle)
+
+    if (x + z) >= mapx + mapz/2 and y <= mapy + mapt/2:
+        # Se estiver no quadrante 2, chamada recursiva
+        answer = answer | search_map(amap[1], (mapx + mapz/2, mapy, mapz/2, mapt/2), search_rectangle)
+
+    if (x + z) >= mapx + z/2 and (y + t) >= mapy + mapt/2:
+        # Se estiver no quadrante 3, chamada recursiva
+        answer = answer | search_map(amap[2], (mapx + mapz/2, mapy + mapt/2, mapz/2, mapt/2), search_rectangle)
+
+    if x <= mapx + mapz/2 and (y + t) >= mapy + mapt/2:
+        # Se estiver no quadrante 4, chamada recursiva
+        answer = answer | search_map(amap[3], (mapx, mapy + mapt/2, mapz/2, mapt/2), search_rectangle)
+
+    return set(answer)
+
+map_rectangle = (0, 0, 735, 959)
+search_retangle = (417, 697, 316, 238)
+
+print(search_map(('A', (None, None, 'E', 'F'), ('D', None, None, 'B'), 'C'), map_rectangle, search_retangle))
