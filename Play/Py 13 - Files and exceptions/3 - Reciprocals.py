17164d2c492a5a68e6e4d10f75e6e260d641ec6f
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-28 18:08:04 +0000

    Update 3 - Reciprocals.py

--- Play/Py 13 - Files and exceptions/3 - Reciprocals.py
+++ Play/Py 13 - Files and exceptions/3 - Reciprocals.py
@@ -1,2 +1,20 @@
 # Created on January, 2021
 # @author: Fábio Araújo de Sá
+
+def reciprocals(alist):
+
+    answer = []
+
+    for item in alist:
+
+        try:
+            number = 1/item
+            answer.append(round(number, 3))
+        
+        except TypeError:
+            continue
+            
+        except ZeroDivisionError:
+            continue
+
+    return answer
