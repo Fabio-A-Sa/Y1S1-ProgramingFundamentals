858a8adf5535ec922e760f32807d69b01ea68f74
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-28 18:01:50 +0000

    Update 1 - Division.py

--- Play/Py 13 - Files and exceptions/1 - Division.py
+++ Play/Py 13 - Files and exceptions/1 - Division.py
@@ -1,2 +1,14 @@
 # Created on January, 2021
 # @author: Fábio Araújo de Sá
+
+def division(a, b):
+
+    try:
+        number = a/b
+        return "{}/{} = {}".format(a, b, number)
+
+    except ZeroDivisionError:
+        return "can't divide by 0!"
+        
+    except TypeError:
+        return "unsupported operand type(s) for division"
