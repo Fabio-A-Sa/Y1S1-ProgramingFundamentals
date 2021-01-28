ddb812160496645cdfaacd8f95d75d4a022b20b7
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-28 19:17:06 +0000

    Update 6 - Mail Merge.py

--- Play/Py 13 - Files and exceptions/6 - Mail Merge.py
+++ Play/Py 13 - Files and exceptions/6 - Mail Merge.py
@@ -1,2 +1,18 @@
 # Created on January, 2021
 # @author: Fábio Araújo de Sá
+
+def mail_merge(names, mail_template):
+
+    all_emails = []
+
+    with open(names, "r") as nomes:
+        all_names = nomes.readlines()
+
+    with open(mail_template, "r") as email:
+        template = email.read()
+
+    for name in all_names:
+        new_email = template.replace("<name>", name[:-1])
+        all_emails.append(new_email)
+
+    return all_emails
