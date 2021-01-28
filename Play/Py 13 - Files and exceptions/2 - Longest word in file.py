029c0ddeaec77d1a957e523ccde082ce1eceaee0
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-28 18:02:02 +0000

    Update 2 - Longest word in file.py

--- Play/Py 13 - Files and exceptions/2 - Longest word in file.py
+++ Play/Py 13 - Files and exceptions/2 - Longest word in file.py
@@ -1,2 +1,17 @@
 # Created on January, 2021
 # @author: Fábio Araújo de Sá
+
+def longest(filename):
+
+    longest_word = []
+    length = 0
+
+    with open(filename, "r") as this_file:
+        for line in this_file:
+            search = line.split(" ")
+            for word in search:
+                if len(word) > length:
+                    longest_word.append(word)
+                    length = len(word)
+
+    return longest_word[-1]
