8e6f1ac9af6cf2e41259fe1fba20fbc610cc646e
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-28 18:22:50 +0000

    Update 4 - Exception counter.py

--- Play/Py 13 - Files and exceptions/4 - Exception counter.py
+++ Play/Py 13 - Files and exceptions/4 - Exception counter.py
@@ -1,2 +1,18 @@
 # Created on January, 2021
 # @author: Fábio Araújo de Sá
+
+def count_exceptions(f, n1, n2):
+
+    counter = 0
+    integers = list([x for x in range(n1, n2+1)])
+
+    for number in integers:
+
+        try:
+            f(number)
+
+        except: # Any error
+            counter = counter + 1
+            continue
+
+    return counter
