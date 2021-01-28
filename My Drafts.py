522c24cd04eb63876222ca3a3ecdd95c8933fc57
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-28 18:22:17 +0000

    Update My Drafts.py

--- My Drafts.py
+++ My Drafts.py
@@ -8949,3 +8949,19 @@ def reciprocals(alist):
             continue
 
     return answer
+
+def count_exceptions(f, n1, n2):
+
+    counter = 0
+    integers = list([x for x in range(n1, n2+1)])
+
+    for number in integers:
+
+        try:
+            f(number)
+
+        except: # Any error
+            counter = counter + 1
+            continue
+
+    return counter
