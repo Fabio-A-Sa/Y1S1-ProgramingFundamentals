719887a9fd91c3470eceae5439a1501aeaf33d38
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-28 21:02:21 +0000

    Update My Drafts.py

--- My Drafts.py
+++ My Drafts.py
@@ -9022,3 +9022,43 @@ def diff(filename1, filename2):
             all_lines.append(local_line)
 
     return all_lines
+      
+def diff(filename1, filename2):
+
+    all_lines = []
+    mais = "+ "
+    menos = "- "
+
+    with open(filename1, "r") as f1:
+        lines_1 = list([line for line in f1.readlines()])
+
+    with open(filename2, "r") as f2:
+        lines_2 = list([line for line in f2.readlines()])
+
+    for line1 in lines_1:
+        for line2 in lines_2:
+
+            if lines_1.index(line1) == lines_2.index(line2):
+
+                local_line = ""
+
+                if line1 not in lines_2:
+                    local_line = local_line + menos + line1
+                    all_lines.append(local_line)
+
+    for line1 in lines_1:
+        for line2 in lines_2:
+
+            if lines_1.index(line1) == lines_2.index(line2):
+
+                local_line = ""
+
+                if line2 not in lines_1:
+                    local_line = local_line + mais + line2
+                    all_lines.append(local_line)
+
+    result = ""
+    for item in all_lines:
+        result = result + item
+    
+    return result
