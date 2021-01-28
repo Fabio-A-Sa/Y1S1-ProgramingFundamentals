d9a83dc60cfb73aeec1727ce537db9987568f49e
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-28 18:45:27 +0000

    Update My Drafts.py

--- My Drafts.py
+++ My Drafts.py
@@ -8965,3 +8965,21 @@ def count_exceptions(f, n1, n2):
             continue
 
     return counter
+
+      def interleave(f1, f2):
+
+    result = ""
+
+    with open(f1, "r") as file_1:
+        with open(f2, "r") as file_2:
+            
+            lines_f1 = list([line for line in file_1])
+            lines_f2 = list([line for line in file_2])
+
+            for l1 in lines_f1:
+                for l2 in lines_f2:
+
+                    if lines_f1.index(l1) == lines_f2.index(l2):
+                        result = result + l1 + l2
+    
+    return result
