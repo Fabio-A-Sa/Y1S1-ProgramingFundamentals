a5216612e0aa2335d55de1ffc1ec1b3e9ec59b6e
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-29 23:12:15 +0000

    Update My Drafts.py

--- My Drafts.py
+++ My Drafts.py
@@ -9215,3 +9215,49 @@ def fib(start, end):
 start = 10
 end = 40
 print(fib(10, 40))
+
+      
+import time
+
+def rec_fib(n, fib_values):
+
+    if n in fib_values.keys():
+        return fib_values[n]
+
+    else:
+        fib_values[n] = rec_fib(n-1, fib_values) + rec_fib(n-2, fib_values)
+        return fib_values[n]
+
+    
+def fib(start, end):
+    
+    all_numbers = []
+
+    fib_values = {
+        
+        1 : 1,
+        2 : 1,
+        3 : 2, 
+
+    }
+
+    total = 0
+    t0 = time.perf_counter()
+
+    for n in range(start, end+1):
+        print("Número {} da Sequência de Fibonacci --> {}".format(n, rec_fib(n, fib_values)))
+        t1 = time.perf_counter()
+        total = total + t1
+        print("Tempo que demorou: {} segundos".format(round(t1-t0, 2)))
+        print(" ")
+        all_numbers.append(rec_fib(n, fib_values))
+    
+    t1 = time.perf_counter()
+    
+    print(" ")
+    return "Processo finalizado. Tempo total utilizado: {} segundos!".format(round(total, 2))
+    
+931487 segundos = 10 dias aproximadamente!
+start = 1
+end = 10000
+print(fib(start, end))
