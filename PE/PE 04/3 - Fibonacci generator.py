1473503c55255f2b83e1a10390d3696191c24bd9
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-29 16:54:31 +0000

    Update 3 - Fibonacci generator.py

--- PE/PE 04/3 - Fibonacci generator.py
+++ PE/PE 04/3 - Fibonacci generator.py
@@ -1,2 +1,26 @@
 # Created on January, 2021
 # @author: Fábio Araújo de Sá
+
+def rec_fib(n):
+    
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    if n == 3:
+        return 2
+    else:
+        return rec_fib(n-1) + rec_fib(n-2)
+    
+    
+def fib(start, end):
+    
+    all_numbers = []
+    
+    for n in range(start, end+1):
+        all_numbers.append(rec_fib(n))
+    
+    for number in all_numbers:
+        yield number
+
+# JCL's better solution:
