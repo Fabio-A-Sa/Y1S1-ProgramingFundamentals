8044f9f2a1ef6199d67ca8738ab9765bf252f19c
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-29 16:53:17 +0000

    Update 1 - Lists to dictionary.py

--- PE/PE 04/1 - Lists to dictionary.py
+++ PE/PE 04/1 - Lists to dictionary.py
@@ -1,2 +1,15 @@
 # Created on January, 2021
 # @author: Fábio Araújo de Sá
+
+def lists_to_dict(list1, list2):
+    
+    result = {}
+    
+    for key in list1:
+        for value in list2:
+            
+            if list1.index(key) == list2.index(value):
+                
+                result[key] = value
+                
+    return result
