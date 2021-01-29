6e346c497d5b8bcab49008ee50a6c9dfb370fd00
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-29 16:55:10 +0000

    Update 4 - Overlap segments.py

--- PE/PE 04/4 - Overlap segments.py
+++ PE/PE 04/4 - Overlap segments.py
@@ -1,2 +1,27 @@
 # Created on January, 2021
 # @author: Fábio Araújo de Sá
+
+def overlaps(segments):
+    
+    result = []
+    
+    for i in range(len(segments)):
+        
+        for j in range(i, len(segments)):
+            
+            if j > i:
+                
+                alist1 = [x for x in range(segments[i][0], segments[i][1])]
+                alist2 = [x for x in range(segments[j][0], segments[j][1])]
+                local_tuple = ()
+                
+                for number in alist2:
+                    if number in alist1:
+                        
+                        # Interseção!
+                        local_tuple = i, j
+                        result.append(local_tuple)
+
+    return set(result)
+  
+# JCL's better solution:
