6432006d2e537cbf9212b2bac93dab9ecf3798b5
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-29 23:18:43 +0000

    Update 5 - Process commands.py

--- PE/PE 04/5 - Process commands.py
+++ PE/PE 04/5 - Process commands.py
@@ -17,3 +17,46 @@ def rec_hof(hofs, lst):
       
 # JCL's better solution:
 
+# alternative solution 1
+def rec_hof(hofs, lst):
+
+    if hofs == []:
+    # base case, there's no more functions to apply
+        return lst
+
+    # get the transformed sub-lists
+    aux = [rec_hof(hofs[1:], x) for x in lst]
+    # apply the first higher-order function to the sub-lists
+    (f_op, f_arg) = hofs[0]
+    return f_op(f_arg, aux)
+
+# alternative solution 2
+def rec_hof2(hofs, lst):
+
+    if hofs == []:
+    # base case, there's no more functions to apply
+        return lst
+
+    # get the transformed sub-lists
+    aux = []
+    for l in lst:
+        aux.append(rec_hof(hofs[1:], l))
+        # apply the first higher-order function to the sub-lists
+        (f_op, f_arg) = hofs[0]
+
+    return f_op(f_arg, aux)
+
+# alternative solution 3
+import functools
+def rec_hof3(hofs, lst):
+
+    if hofs == []:
+    # base case, there's no more functions to apply
+        return lst
+
+    # get the transformed sub-lists using partial function application
+    aux = map(functools.partial(rec_hof, hofs[1:]), lst)
+    # apply the first higher-order function to the sub-lists
+    (f_op, f_arg) = hofs[0]
+
+    return f_op(f_arg, aux)
