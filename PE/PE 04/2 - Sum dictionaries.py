93fd93bcf7359d9d45b665f8779d5a8bfcc14567
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-29 16:53:39 +0000

    Update 2 - Sum dictionaries.py

--- PE/PE 04/2 - Sum dictionaries.py
+++ PE/PE 04/2 - Sum dictionaries.py
@@ -1,2 +1,19 @@
 # Created on January, 2021
 # @author: Fábio Araújo de Sá
+
+def sum_dicts(lst):
+    
+    result = {}
+    
+    for dictionary in lst:
+        for key in dictionary:
+            
+            result[key] = 0
+    
+    for dictionary in lst:
+        for key in dictionary:
+            
+            result[key] = result[key] + dictionary[key]
+            
+            
+    return result
