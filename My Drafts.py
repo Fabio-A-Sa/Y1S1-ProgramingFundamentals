d053fb47db9febe657af903c8f42162cf0d2cf2a
Fábio Sá <72668966+Fabio-A-Sa@users.noreply.github.com>
2021-01-30 11:46:29 +0000

    Update My Drafts.py

--- My Drafts.py
+++ My Drafts.py
@@ -9327,3 +9327,25 @@ def diff(filename1, filename2):
     return delta[26:]
 
 print(len("--- \n+++ \n@@ -1,5 +1,5 @@\n"))
+
+from difflib import unified_diff as df
+# https://docs.python.org/3/library/difflib.html
+
+def diff(filename1, filename2):
+
+    file1 = open(filename1, 'r').readlines()
+    one = list([" " + line for line in file1])
+    file2 = open(filename2, 'r').readlines()
+    two = list([" " + line for line in file2])
+
+    difference = list(df(one, two, fromfile = "file1", tofile = "file2", lineterm=""))
+    
+    for line in difference:
+        if line[0] == " ":
+            difference.remove(line)
+
+    acumulator = ""
+    for line in difference:
+        acumulator = acumulator + line
+
+    return acumulator[33:]
