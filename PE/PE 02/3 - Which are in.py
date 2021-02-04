299977b2f8c2b84857c431904488b8972a6c5808
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-04 19:01:00 +0000

    Lines

---
+++ PE/PE 02/3 - Which are in.py
@@ -0,0 +1,16 @@
+# Created on December, 2020
+# @author: Fábio Araújo de Sá
+
+def which_are_in(l1, l2):
+
+    result = []
+
+    for item1 in l1:
+        for item2 in l2:
+            if item1 in item2:
+                if item1 not in result:
+                    result.append(item1)
+
+
+
+    return sorted(result)
