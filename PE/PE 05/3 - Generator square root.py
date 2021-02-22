a3ab7345bf9b544f0171be0b9b90fd79440d5420
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-22 21:16:50 +0000

    PE05 3

--- PE/PE 05/3 - Generator square root.py
+++ PE/PE 05/3 - Generator square root.py
@@ -1,2 +1,29 @@
 # Created on February, 2021
 # @author: Fábio Araújo de Sá
+
+def sqrt(num, k):
+
+    answer_list = []
+    auxiliar = num
+    delta = 100
+
+    # List constructor
+    initial = auxiliar / 2
+    answer_list.append(initial)
+    while len(answer_list) != k:
+
+        next = (initial + auxiliar/initial)/2
+        answer_list.append(round(next, 2))
+        delta = initial - next
+        initial = next
+        if delta > 0.0001:
+            continue
+
+    # Generator
+    for number in answer_list:
+        yield number
+
+
+# num = 20
+# k = 6
+# print(sqrt(num, k)) 
