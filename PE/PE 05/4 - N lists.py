6a9dfc920ce024e33b635654e9253ec2925a0b75
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-22 22:25:35 +0000

    PE05 4

---
+++ PE/PE 05/4 - N lists.py
@@ -0,0 +1,29 @@
+# Created on February, 2021
+# @author: Fábio Araújo de Sá
+
+def n_lists(alist, n):
+    
+    for thing in alist:
+        if type(thing[0]) is int:
+        
+            new = []
+            for sublist in alist:
+
+                if len(sublist) <= n:
+                    new.append(sublist)
+
+                else:
+                    while len(sublist) > n:
+                        new.append(sublist[:n])
+                        sublist = sublist[n:]
+                    new.append(sublist)
+            
+            return new
+
+        else: # it's a list --> recursive search
+            return list(n_lists(thing, n))
+
+
+alist = [[0], [1, 2, 3, 8, 9, 10], [5, 6]]
+n = 2
+print(n_lists(alist, n))
