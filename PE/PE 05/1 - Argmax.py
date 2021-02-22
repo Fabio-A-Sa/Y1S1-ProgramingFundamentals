d0aac34e4da81bf9d2da5c8554efccdab29f6ed2
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-02-22 20:27:29 +0000

    PE05 1

--- PE/PE 05/1 - Argmax.py
+++ PE/PE 05/1 - Argmax.py
@@ -1,2 +1,22 @@
 # Created on February, 2021
 # @author: Fábio Araújo de Sá
+
+def argmax1(lst):
+    return (lst.index(max(lst)))
+
+def argmax2(lst):
+
+    maior = -10000000
+    for number in lst:
+        if number > maior:
+            maior = number
+
+    for index in range(len(lst)):
+        if maior == lst[index]:
+            return index
+        index = index + 1
+
+    return None
+
+alist = [1, 10, 1, 2, 10]
+print(argmax2(alist))
