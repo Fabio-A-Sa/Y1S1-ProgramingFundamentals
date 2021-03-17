931224bc3aa784edfddd808695c88fb1fc1329e2
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-17 16:40:37 +0000

    Brute Force with coordinates

--- Mystery PNPG.py
+++ Mystery PNPG.py
@@ -1,6 +1,6 @@
 # Part 1 -> Decrypt message using a = 0 until j = 9
 
-def decrypt1 (message):
+def decrypt (message):
 
     from string import ascii_lowercase as abc
     for caracter in message:
@@ -23,37 +23,9 @@ def decrypt1 (message):
             total_2 = 0
             continue
 
-    print("Total = {}\nPrimeira parte = {}\nSegunda Parte = {}".format(round(total_1 + total_2, 3), round(total_1, 3), round(total_2, 3)))
+    print("Somatório total = {}\nPrimeira parte, antes do '+' = {}\nSegunda Parte, depois do '+' = {}".format(round(total_1 + total_2, 3), round(total_1, 3), round(total_2, 3)))
 
     return numbers
-
-def decrypt2 (message):
-
-    from string import ascii_lowercase as abc
-    for caracter in message:
-        if caracter in abc:
-            message = message.replace(caracter, str((abc.find(caracter)+1)%10))
-        else:
-            continue
-    
-    total_1 = 0
-    total_2 = 0
-    numbers = []
-    for par in message.split(" "):
-
-        try:
-            print(float(par))
-            numbers.append(float(par))
-            total_2 = total_2 + float(par)
-        except ValueError:
-            total_1 = total_2
-            total_2 = 0
-            continue
-
-    print("Total = {}\nPrimeira parte = {}\nSegunda Parte = {}".format(round(total_1 + total_2, 3), round(total_1, 3), round(total_2, 3)))
-
-    return numbers
-
 
 def get_coordinates ( alist ) :
 
@@ -62,12 +34,13 @@ def get_coordinates ( alist ) :
     available = []
 
     for number in alist:
-        if len(str(rd(number))) == 2 and rd(number) < 60:
+        if (len(str(rd(number))) == 2) and rd(number) < 60:
             if len(str(number)) == 5:
                 string = str(number) + "0"
                 available.append(string)
             else:
                 available.append(str(number))
+
         else:
             continue
     
@@ -78,10 +51,12 @@ def get_coordinates ( alist ) :
         attemp = n + norte + w + oeste
         possibilities.append(attemp)
 
+    for pos in possibilities:
+        print(pos)
     return possibilities
 
 code = "bch.ga dc.ag bia.jeh bf.jjj hc.gda bf.jjj ea.ahi bc.abb b.aai bbe.ibi gj.hcd cg.jib bcg.jae he.jcb + eh.igh fi.jdd"
-numbers = decrypt1(code)
+numbers = decrypt(code)
 print(numbers)
 print(get_coordinates(numbers))
 
