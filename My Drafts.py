c2f2247353086878af88b6083337ce587dab6194
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-18 09:05:45 +0000

    Drafts update

--- My Drafts.py
+++ My Drafts.py
@@ -10295,10 +10295,64 @@ def decrypt2 (message):
             total_2 = 0
             continue
 
-    print("Total = {}\nPrimeira parte = {}\nSegunda Parte = {}".format(round(total_1 + total_2, 3), round(total_1, 3), round(total_2, 3)))
+# Part 1 -> Decrypt message using a = 0 until j = 9
+
+def decrypt (message):
+
+    from string import ascii_lowercase as abc
+    for caracter in message:
+        if caracter in abc:
+            message = message.replace(caracter, str(abc.find(caracter)))
+        else:
+            continue
+    
+    total_1 = 0
+    total_2 = 0
+    numbers = []
+    for par in message.split(" "):
+
+        try:
+            numbers.append(float(par))
+            print(float(par))
+            total_2 = total_2 + float(par)
+        except ValueError:
+            total_1 = total_2
+            total_2 = 0
+            continue
+
+    print("Somatório total = {}\nPrimeira parte, antes do '+' = {}\nSegunda Parte, depois do '+' = {}".format(round(total_1 + total_2, 3), round(total_1, 3), round(total_2, 3)))
+
+    return numbers
+
+def get_coordinates ( alist ) :
+
+    from math import floor as rd 
+    possibilities = []
+    available = []
+
+    for number in alist:
+        if (len(str(rd(number))) == 2) and rd(number) < 60:
+            if len(str(number)) == 5:
+                string = str(number) + "0"
+                available.append(string)
+            else:
+                available.append(str(number))
+
+        else:
+            continue
+    
+    n = "N 41 "
+    w = " W 008 "
+    combinations = [(x, y) for x in available for y in available]
+    for norte, oeste in combinations:
+        attemp = n + norte + w + oeste
+        possibilities.append(attemp)
+
+    for pos in possibilities:
+        print(pos)
+    return possibilities
 
 code = "bch.ga dc.ag bia.jeh bf.jjj hc.gda bf.jjj ea.ahi bc.abb b.aai bbe.ibi gj.hcd cg.jib bcg.jae he.jcb + eh.igh fi.jdd"
-print(decrypt1(code))
-print(decrypt2(code))
- 
-
+numbers = decrypt(code)
+print(numbers)
+print(get_coordinates(numbers))
