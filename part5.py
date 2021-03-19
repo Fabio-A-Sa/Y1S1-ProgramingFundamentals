45017b0d206349a169c7156389954bdb06776803
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-19 21:57:30 +0000

    Add instructions

--- part5.py
+++ part5.py
@@ -1,6 +1,6 @@
 from string import ascii_uppercase as abc
 
-instructions = "Para Santiago por vezes segue-se o azul e só depois o amarelo. Nas termas recupera-se melhor a cantar"
+instructions = "Para Santiago por vezes segue-se o azul e só depois o amarelo. Nas termas recupera-se melhor a cantar."
 message1 = "9-13-1-6-21-18-9-19"
 message2 = "19-9-13-15-4-1-12-9-19-21-9-19"
 message3 = "12-9-18-9-19-1-12-15-4"
@@ -14,7 +14,19 @@ def frequency (message):
     dictionary = {}
     for letter in message:
         if letter not in dictionary.keys():
-            
+            dictionary[letter] = dictionary.get(letter, 0) + 1
+        else:
+            dictionary[letter] += 1
+
+    return dictionary
+
+def sum_freqs (random):
+
+    main_dict = {}
+
+
+
+    return main_dict
 
 a1 = [int(x) for x in sorted(message1.split("-"), key = lambda x: int(x))]
 print(a1, sum(a1))
@@ -31,5 +43,8 @@ print(a3, sum(a3))
 for item in a3:
     print(abc[item-1])
 
-for item in [m1, m2, m3]:
-    print(frequency(item))
+all_messages = [m1, m2, m3]
+for message in all_messages:
+    print(frequency(message))
+
+
