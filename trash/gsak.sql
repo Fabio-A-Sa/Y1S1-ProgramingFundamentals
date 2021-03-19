d39a8be740f291665a3d7bbf45140be19bad7251
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-19 23:17:11 +0000

    Macro sql gsak

--- trash/gsak.sql
+++ trash/gsak.sql
@@ -37,3 +37,8 @@ while not($_sqleol)
   $data = $data + sqlget("code") + sqlget("name") + sqlget("userdata") + $_Crlf
   sqlnext
 Endwhile
+
+// today message
+
+$sqlToday = DateToSql($_Today)
+msgok msg=$SqlToday
