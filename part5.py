cf3615aadaa1d85dc381604058eb024104c2ed2c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-19 22:11:41 +0000

    Add frequency

--- part5.py
+++ part5.py
@@ -14,10 +14,15 @@ def to_letters(numbers, offset):
 
     return astring
 
-# A = 0
-m1 = to_letters(message1, 0)
-m2 = to_letters(message2, 0)
-m3 = to_letters(message3, 0)
+# A == 1
+m1 = to_letters(message1, 1) # JNBGVSJT
+m2 = to_letters(message2, 1) # TJNPEBMJTVJT
+m3 = to_letters(message3, 1) # MJSJTBMPE
+
+# A == 0
+m1 = to_letters(message1, 0) # IMAFURIS
+m2 = to_letters(message2, 0) # SIMODALISUIS
+m3 = to_letters(message3, 0) # LIRISALOD
 
 def frequency (message):
 
@@ -30,35 +35,41 @@ def frequency (message):
 
     return dictionary
 
-def sum_freqs (random):
+def sum_freqs (rd):
 
     main_dict = {}
-
-
+    
+    for dictionary in rd:
+        for key in dictionary:
+            
+            main_dict[key] = 0
+    
+    for dictionary in rd:
+        for key in dictionary:
+            
+            main_dict[key] = main_dict[key] + dictionary[key]
 
     return main_dict
 
 a1 = [int(x) for x in sorted(message1.split("-"), key = lambda x: int(x))]
 print(a1, sum(a1))
-for item in a1:
-    print(abc[item-0])
 
 a2 = [int(x) for x in sorted(message2.split("-"), key = lambda x: int(x))]
 print(a2, sum(a2))
-for item in a2:
-    print(abc[item-1])
 
 a3 = [int(x) for x in sorted(message3.split("-"), key = lambda x: int(x))]
 print(a3, sum(a3))
-for item in a3:
-    print(abc[item-1])
 
 all_messages = [m1, m2, m3]
 dicts = []
 for message in all_messages:
     print(frequency(message))
+    # {'I': 2, 'M': 1, 'A': 1, 'F': 1, 'U': 1, 'R': 1, 'S': 1}
+    # {'S': 3, 'I': 3, 'M': 1, 'O': 1, 'D': 1, 'A': 1, 'L': 1, 'U': 1}
+    # {'L': 2, 'I': 2, 'R': 1, 'S': 1, 'A': 1, 'O': 1, 'D': 1}
     dicts.append(frequency(message))
 
 print("\nTotal sum: {}".format(sum_freqs(dicts)))
+# Total sum: {'I': 7, 'M': 2, 'A': 3, 'F': 1, 'U': 2, 'R': 2, 'S': 5, 'O': 2, 'D': 2, 'L': 3}
 
 
