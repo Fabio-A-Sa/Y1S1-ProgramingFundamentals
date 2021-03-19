21ae7f1febf837a2828c73e921b46bd5c8615e64
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-19 23:10:39 +0000

    Macro sql gsak

--- gsak.sql
+++ gsak.sql
@@ -7,8 +7,3 @@ while not($_sqleol)
   $data = $data + sqlget("code") + sqlget("name") + sqlget("userdata") + $_Crlf
   sqlnext
 Endwhile
-
-// Filter by dificulty
-
-$data = sqlite("sql","Select * from caches where difficulty = $d_difficulty","")
-
