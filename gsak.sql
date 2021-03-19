d88b6137811cdb9151e9bc80fc1497885cfccff5
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-19 23:10:34 +0000

    Macro sql gsak

--- gsak.sql
+++ gsak.sql
@@ -1,6 +1,14 @@
+
+// Filter by status
+
 $status = sqlite("sql",$_SqlGrid, "sqlget=yes")
 $data = ""
 while not($_sqleol)
   $data = $data + sqlget("code") + sqlget("name") + sqlget("userdata") + $_Crlf
   sqlnext
 Endwhile
+
+// Filter by dificulty
+
+$data = sqlite("sql","Select * from caches where difficulty = $d_difficulty","")
+
