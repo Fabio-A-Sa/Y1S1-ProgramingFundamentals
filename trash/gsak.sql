965f35cf3928ca19cc1bc4150fd170b7a5d7e8fa
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-19 23:16:17 +0000

    Macro sql gsak

--- trash/gsak.sql
+++ trash/gsak.sql
@@ -19,3 +19,13 @@ $test = "$d_Difficulty"
 $data = sqlite("sql","Select * from caches where difficulty = $d_difficulty","")
 
 
+// Order and sorted
+
+$CurrentSort = $_OrderBy
+#Change our sort sequence to do some work in our desired sequence
+SQLSort OrderBy=Terrain Asc, Difficulty desc
+# do some work here
+#....
+# Now finally restore our current sort sequence
+SQLSort OrderBy=$CurrentSort
+
