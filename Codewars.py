e42a014ae4999a4b31262610d68392fe8ba4d320
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-28 20:39:53 +0100

    Challenge

--- Codewars.py
+++ Codewars.py
@@ -1,7 +1,17 @@
 # Code wars challenges using Python
 
 def generate_hashtag(s):
-    return s.split(" ")
+
+    if len(s) == 0 or len(s) > 140:
+        return False
+    else:
+        words = [word.capitalize() for word in s.split(" ") if word != " "]
+        hastag = "#"
+        for word in words:
+            hastag += word
+
+        return hastag
 
 s = "    Hello     World   "
 print(generate_hashtag(s))
+
