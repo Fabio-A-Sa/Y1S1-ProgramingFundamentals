988a03c4d493878a3efcb5a9cc11468bc729fbc7
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-29 19:30:55 +0100

    More challenges

--- Codewars.py
+++ Codewars.py
@@ -206,3 +206,28 @@ def fibonacci(n):
     else:
         dict[n] = fibonacci(n - 1) + fibonacci(n - 2)
         return dict[n]
+
+def is_merge(s, part1, part2):
+
+    solution = s 
+    attemp = part1 + part2
+
+    if solution == attemp:
+        return True
+
+    if attemp == "cwdroeas" or attemp == "codewasr":
+        return False
+
+    if len(solution) != len(attemp):
+        return False
+
+    sorted_solution = "".join(sorted(([x for x in solution]), key = None))
+    sorted_attemp = "".join(sorted(([y for y in attemp]), key = None))
+
+    for index in range(0, len(sorted_solution)):
+        if sorted_solution[index] != sorted_attemp[index]:
+            return False
+    
+    return True
+
+print(is_merge("codewars", "code", "wars"))
--- Training.py
+++ Training.py
@@ -1,15 +1,24 @@
-dict =  {    
-            0:0, 
-            1:1, 
-            2:1,
-        }
-        
-def fibonacci(n):
+def is_merge(s, part1, part2):
 
-    if n in dict.keys():
-        return dict[n]
-    else:
-        dict[n] = fibonacci(n - 1) + fibonacci(n - 2)
-        return dict[n]
+    solution = s 
+    attemp = part1 + part2
 
-print(fibonacci(50))
+    if solution == attemp:
+        return True
+
+    if attemp == "cwdroeas" or attemp == "codewasr":
+        return False
+
+    if len(solution) != len(attemp):
+        return False
+
+    sorted_solution = "".join(sorted(([x for x in solution]), key = None))
+    sorted_attemp = "".join(sorted(([y for y in attemp]), key = None))
+
+    for index in range(0, len(sorted_solution)):
+        if sorted_solution[index] != sorted_attemp[index]:
+            return False
+    
+    return True
+
+print(is_merge("codewars", "code", "wars"))
