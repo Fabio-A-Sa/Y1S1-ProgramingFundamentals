6b3a475af92b9176af1f83ad5d84df080a9aeff7
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-03-29 20:06:37 +0100

    More challenges

--- Training.py
+++ Training.py
@@ -1,28 +1,31 @@
-def binary_to_decimal(string):
+def mixed_fraction(s):
+    
+    from math import gcd
+    numbers = [int(x) for x in s.split("/")]
 
-    from math import pow
-    exponent = 0
-    decimal = 0
+    try:
 
-    while (exponent != len(string)):
+        r = numbers[0] // numbers[1]
+        numbers[0] = numbers[0] - r * numbers[1]
+        divisor = gcd(numbers[0], numbers[1])
+        numbers[0] = round(numbers[0] / divisor)
+        numbers[1] = round(numbers[1] / divisor)
 
-        digit = int(string[exponent])
-        decimal = decimal + pow(2, exponent)*digit
-        exponent += 1
+        if numbers[0] * numbers[1] > 0:
+            numbers[0] = abs(numbers[0])
+            numbers[1] = abs(numbers[1])
+            solution = "{} {}/{}".format(str(r), str(numbers[0]), str(numbers[1]))
 
-    return round(decimal)
+        elif numbers[0] * numbers[1] == 0:
+            solution = "{}".format(str(r))
+
+        elif r == 0:
+            solution = "{}/{}".format(str(numbers[0]), str(numbers[1]))
+
+        return solution
+        
+    except ZeroDivisionError:
+        raise ZeroDivisionError
 
 
-def pattern (string):
-
-    available = ['0', '1']
-    number = ""
-    for char in string:
-        number = char + number
-        if char not in available:
-            return False
-
-    decimal = binary_to_decimal(number)
-    return decimal % 3 == 0
-
-print(pattern("111"))
+print(mixed_fraction("42/9"))
