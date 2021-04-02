81b8c5eacff2aa7db0ead7975676690758f064fd
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-02 11:07:08 +0100

    Challenge

--- Codewars.py
+++ Codewars.py
@@ -348,3 +348,27 @@ nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
 on Sundays, made away with three-quarters of his income."""
 
 print(top_3_words(text))
+
+def next_bigger(n):
+
+    from itertools import permutations
+
+    numbers = []
+    for number in str(n):
+        numbers.append(str(number))
+
+    all_cases = list(permutations(numbers))
+    new_numbers = []
+    for pair in all_cases:
+        string = ""
+        for item in pair:
+            string = string + item
+        new_numbers.append(int(string))
+
+    new_numbers = sorted(new_numbers, key = lambda x : x)
+    for number in new_numbers:
+        if number > n:
+            return number
+
+n = 2021
+print(next_bigger(n))
