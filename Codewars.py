331c4a2747659e940d2c6629fe6efef5fe47ff0f
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-03 19:53:24 +0100

    Challenge 6

--- Codewars.py
+++ Codewars.py
@@ -402,3 +402,21 @@ def converter(mpg):
 
 def replace_dots(str):
     return str.replace(".", "-")
+
+def human_years_cat_years_dog_years(human_years):
+    
+    catYears = 0
+    dogYears = 0
+
+    for year in range (1, human_years+1):
+        if year == 1:
+            dogYears += 15
+            catYears += 15
+        elif year == 2:
+            dogYears += 9
+            catYears += 9
+        else:
+            dogYears += 5
+            catYears += 4
+
+    return [human_years, catYears, dogYears]
--- Training.py
+++ Training.py
@@ -1,2 +1,17 @@
-def replace_dots(str):
-    return str.replace(".", "-")
+def human_years_cat_years_dog_years(human_years):
+    
+    catYears = 0
+    dogYears = 0
+
+    for year in range (1, human_years):
+        if year == 1:
+            dogYears += 15
+            catYears += 15
+        elif year == 2:
+            dogYears += 9
+            catYears += 9
+        else:
+            dogYears += 5
+            catYears += 4
+
+    return [human_years, catYears, dogYears]
