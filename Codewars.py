4086fc9589c150dfe3a2915b57c56728394b5bd9
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-03 19:31:09 +0100

    Challenge 2

--- Codewars.py
+++ Codewars.py
@@ -381,3 +381,14 @@ def generate_range(min, max, step):
 
 def mouth_size(animal): 
   return "small" if animal.lower() == "alligator" else "wide"
+
+def quarter_of(month):
+  
+    if month in [1, 2, 3]:
+      return 1
+    elif month in [4, 5, 6]:
+      return 2
+    elif month in [7, 8, 9]:
+      return 3
+    else:
+      return 4
--- Training.py
+++ Training.py
@@ -1,2 +1,10 @@
-def mouth_size(animal): 
-  return "small" if animal.lower() == "aligator" else "wide"
+def quarter_of(month):
+  
+    if month in [1, 2, 3]:
+      return 1
+    elif month in [4, 5, 6]:
+      return 2
+    elif month in [7, 8, 9]:
+      return 3
+    else:
+      return 4
