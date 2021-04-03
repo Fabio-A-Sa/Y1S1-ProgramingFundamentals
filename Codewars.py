e4ca29015900221781a38b117ea34ab7c903548c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-03 20:18:09 +0100

    Challenge 10

--- Codewars.py
+++ Codewars.py
@@ -453,3 +453,15 @@ def logical_calc(array, op):
               FLAG = FLAG ^ array[index]
            
     return FLAG
+
+def pipe_fix(nums):
+
+    nums = sorted(nums, key = None)
+    minimum = nums[0]
+    maximum = nums[-1] + 1
+
+    solution = []
+    for number in range(minimum, maximum):
+        solution.append(number)
+  
+    return solution
--- Training.py
+++ Training.py
@@ -1,23 +1,11 @@
-def logical_calc(array, op):
+def pipe_fix(nums):
+  
+    nums = sorted(nums, key = None)
+    minimum = nums[0]
+    maximum = nums[-1] + 1
 
-    if op == "AND":
-        FLAG = True
-        for boolean in array:
-            FLAG = FLAG and boolean
-
-    elif op == "OR":
-        FLAG = False
-        for boolean in array:
-            FLAG = FLAG or boolean
-
-    else:
-        if len(array) == 1:
-            FLAG = array[0]
-        elif len(array) == 2:
-            FLAG = array[0] ^ array[1]  
-        else:
-            FLAG = array[0] ^ array[1]  
-            for index in range (2, len(array)):
-              FLAG = FLAG ^ array[index]
-           
-    return FLAG
+    solution = []
+    for number in range(minimum, maximum):
+        solution.append(number)
+  
+  return solution
