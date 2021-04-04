2042f8ce156b4161cbad5e0650eec0751aea056f
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-04 12:50:09 +0100

    Challenge11

--- My Drafts.py
+++ My Drafts.py
@@ -11073,3 +11073,29 @@ def rental_car_cost(d):
         return d*40 - 20
     else:
         return d*40 - 50
+
+def order(sentence):
+
+    def sort_rule (pair):
+        return pair[1]
+
+    all_pairs = []
+    words = sentence.split(" ")
+    for word in words:
+        number = 0
+        for char in word:
+            try:
+                number = int(char)
+            except:
+                continue
+        all_pairs.append((word, number))
+
+    pairs = sorted(all_pairs, key = sort_rule)
+
+    solution = ""
+    for pair in pairs:
+        solution += pair[0] + " "
+
+    return solution.strip()
+
+print(order("4of Fo1r pe6ople g3ood th5e the2"))
--- Training.py
+++ Training.py
@@ -1,14 +1,26 @@
-def spin_words(sentence):
 
+def order(sentence):
+
+    def sort_rule (pair):
+        return pair[1]
+
+    all_pairs = []
     words = sentence.split(" ")
-    solution = ""
     for word in words:
-        if len(word) < 5:
-            solution += word + " "
-        else:
-            compound = ""
-            for char in word:
-                compound = char + compound
-            solution += word + " "
-    
+        number = 0
+        for char in word:
+            try:
+                number = int(char)
+            except:
+                continue
+        all_pairs.append((word, number))
+
+    pairs = sorted(all_pairs, key = sort_rule)
+
+    solution = ""
+    for pair in pairs:
+        solution += pair[0] + " "
+
     return solution.strip()
+
+print(order("4of Fo1r pe6ople g3ood th5e the2"))
