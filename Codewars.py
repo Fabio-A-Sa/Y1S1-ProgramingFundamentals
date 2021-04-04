f55542525f6e106f4e2a416d960ceea5fad1e0a4
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-04 12:03:52 +0100

    Challenge1

--- Codewars.py
+++ Codewars.py
@@ -530,3 +530,22 @@ def sum_mix(arr):
     for number in arr:
         total += int(number)
     return total
+
+def race(v1, v2, g):
+    
+    from math import floor
+    
+    if v2 <= v1:
+        return None
+
+    else:
+
+        time = floor((g / (v2-v1))*60*60)
+        hours = time // (60*60)
+        time = time - hours*60*60
+        minutes = time // 60
+        time = time - minutes*60
+        seconds = time
+        return [hours, minutes, seconds]
+
+print(race(720, 850, 70))
--- Training.py
+++ Training.py
@@ -1,2 +1,17 @@
-def check(seq, elem):
-    return elem in seq
+def race(v1, v2, g):
+    
+    from math import floor
+    if v2 <= v1:
+        return None
+
+    else:
+
+        time = floor((g / (v2-v1))*60*60)
+        hours = time // (60*60)
+        time = time - hours*60*60
+        minutes = time // 60
+        time = time - minutes*60
+        seconds = time
+        return [hours, minutes, seconds]
+
+print(race(720, 850, 70))
