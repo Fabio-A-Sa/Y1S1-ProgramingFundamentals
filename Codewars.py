394ed2ce534af28ccc0729fe5a76f072b5aa6589
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-04 12:29:11 +0100

    Challenge9

--- Codewars.py
+++ Codewars.py
@@ -593,4 +593,12 @@ def dna_to_rna(dna):
 
 def lovefunc( flower1, flower2 ):
     return (flower1+flower2)%2 == 1
+
+def rental_car_cost(d):
     
+    if d < 3:
+        return d*40
+    elif d < 7 and d >=3:
+        return d*40 - 20
+    else:
+        return d*40 - 50
--- Training.py
+++ Training.py
@@ -1,2 +1,8 @@
-def lovefunc( flower1, flower2 ):
-    return (flower1+flower2)%2 == 0
+def rental_car_cost(d):
+    
+    if d < 3:
+        return d*40
+    elif d < 7 and d >=3:
+        return d*40 - 20
+    else:
+        return d*40 - 50
