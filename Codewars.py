a26d6f207fbcc113b4f67358f9c1722ef6f23810
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-04 13:33:17 +0100

    Challenge14

--- Codewars.py
+++ Codewars.py
@@ -664,4 +664,24 @@ def comp(array1, array2):
             return false
 
     return flag
-	
+
+solution = []
+def tourney(inp):
+    
+    solution.append(inp)
+    if len(inp) == 2:
+        solution.append([max(inp)])
+        ret = []
+        for item in solution:
+            if item not in ret:
+                ret.append(item)
+        return ret
+
+    else:
+        sol = []
+        while (len(inp)):
+            sol.append(max(inp[:2]))
+            inp = inp[2:]
+        return tourney(sol)
+
+print(tourney([9, 5, 4, 7, 6, 3, 8]))
