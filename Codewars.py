04304faa26b85d3b79ed965a22e59c0757e4203d
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-10 14:30:07 +0100

    Challenge1

--- Codewars.py
+++ Codewars.py
@@ -723,3 +723,17 @@ def valid_word(seq, word):
             return False
 
     return True if len(seq) else False
+
+from string import ascii_uppercase, ascii_lowercase
+
+def make_upper_case(s):
+
+    solution = ""
+    for char in s:
+        if char in ascii_lowercase:
+            index = ascii_lowercase.find(char)
+            solution += ascii_uppercase[index]
+        else:
+            solution += char
+            
+    return solution
