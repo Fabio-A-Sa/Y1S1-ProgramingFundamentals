a8091e85691b674df839fae8a0e238b6d81d281f
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-10 18:20:21 +0100

    Gitignore update

---
+++ Codewars.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <cmath>
+#include <iostream>
+
+std::string get_middle(std::string input) 
+{   
+    std::string answer = "";
+    if (input.size() % 2 == 0) {
+        int middle = floor(input.size() / 2);
+        answer += input[middle-1] + input[middle];
+    }
+    else {
+        int middle = floor(input.size() / 2); 
+        answer += input[middle];
+    }
+
+    return answer;
+}
+
+int main ()
+{
+    std::cout << get_middle("test") << std::endl;
+    return 0;
+}
