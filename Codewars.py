cf40263002c2a7795b7b0b0f6f5c274a22fa2006
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-10 18:06:59 +0100

    Challenge13

--- Codewars.py
+++ Codewars.py
@@ -1005,3 +1005,44 @@ def to_nato(words):
             continue
     
     return solution.strip()
+
+def to_binary (number):
+
+    string = ""
+    while (number):
+
+        aux = number % 2
+        string = str(aux) + string
+        number = (number - aux) // 2
+
+    return string
+
+def swap(s,n):
+
+    binary = to_binary(n)
+    while (len(binary) < len(s)):
+        binary += binary
+    
+    keys = ""
+    key_counter = 0
+    for i in range(len(s)):
+        if s[i] == " ":
+            keys += " "
+        else:
+            keys += binary[key_counter]
+            key_counter += 1
+
+    solution = ""
+    for index in range(len(s)):
+
+        letter = s[index]
+        if keys[index] == '1':
+            letter = letter.swapcase()
+        solution += letter
+
+    print(keys)
+    print(s)
+    print(solution)
+    return solution
+
+print(swap('the quick broWn fox leapt over the fence', 11))
--- Training.py
+++ Training.py
@@ -1,45 +1,40 @@
-def to_nato(words):
-    
-    dictionary = {
-                    'A' : 'Alfa' ,
-                    'B' : 'Bravo' ,
-                    'C' : 'Charlie' ,
-                    'D' : 'Delta' ,
-                    'E' : 'Echo' ,
-                    'F' : 'Foxtrot' ,
-                    'G' : 'Golf' ,
-                    'H' : 'Hotel' ,
-                    'I' : 'India' ,
-                    'J' : 'Juliett' ,
-                    'K' : 'Kilo' ,
-                    'L' : 'Lima' ,
-                    'M' : 'Mike' ,
-                    'N' : 'November' ,
-                    'O' : 'Oscar' ,
-                    'P' : 'Papa' ,
-                    'Q' : 'Quebec' ,
-                    'R' : 'Romeo' ,
-                    'S' : 'Sierra' ,
-                    'T' : 'Tango' ,
-                    'U' : 'Uniform' ,
-                    'V' : 'Victor' ,
-                    'W' : 'Whiskey' ,
-                    'X' : 'Xray' ,
-                    'Y' : 'Yankee' ,
-                    'Z' : 'Zulu' ,
-                }
-    
-    ponctuation = ['.', '?', '!', ',']
-    solution = ""
+def to_binary (number):
 
-    for char in words:
-        if char.upper() in dictionary.keys():
-            solution += dictionary[char.upper()] + " "
-        elif char in ponctuation:
-            solution += char + " "
+    string = ""
+    while (number):
+
+        aux = number % 2
+        string = str(aux) + string
+        number = (number - aux) // 2
+
+    return string
+
+def swap(s,n):
+
+    binary = to_binary(n)
+    while (len(binary) < len(s)):
+        binary += binary
+    
+    keys = ""
+    key_counter = 0
+    for i in range(len(s)):
+        if s[i] == " ":
+            keys += " "
         else:
-            continue
-    
-    return solution.strip()
+            keys += binary[key_counter]
+            key_counter += 1
 
-print(to_nato('If you! can read'))
+    solution = ""
+    for index in range(len(s)):
+
+        letter = s[index]
+        if keys[index] == '1':
+            letter = letter.swapcase()
+        solution += letter
+
+    print(keys)
+    print(s)
+    print(solution)
+    return solution
+
+print(swap('the quick broWn fox leapt over the fence', 11))
