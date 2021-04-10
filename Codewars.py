32dc3c3e75ebf1b5b3e61f75d49c7aade2279a79
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-10 16:00:38 +0100

    Challenge6

--- Codewars.py
+++ Codewars.py
@@ -803,3 +803,43 @@ def get_participants(handshakes):
         return counter
 
 print(get_participants(6))
+
+def inside_out(st):
+    
+    alist = list([x for x in st.split(" ")]) if len(st) > 1 else [st]
+    string = ""
+
+    for word in alist:
+        
+        result = ""
+
+        if len(word) % 2 == 0:
+
+            left_characters = [word[x] for x in range(0, int(len(word) / 2))]
+            right_characters = [word[y] for y in range(int(len(word) / 2), len(word))]
+
+            for char in left_characters[::-1]:
+                result += char
+            for char in right_characters[::-1]:
+                result += char
+
+        else:
+            
+            if len(word) > 1:
+
+                lose_character = word[int(len(word) / 2)]
+                left_characters = [word[x] for x in range(0, int(len(word) / 2))]
+                right_characters = [word[y] for y in range(int(len(word) / 2) + 1, len(word) )]
+
+                for char in left_characters[::-1]:
+                    result += char
+                result += lose_character
+                for char in right_characters[::-1]:
+                    result += char
+            
+            else:
+                result = word
+
+        string += result + " "
+
+    return string[:len(string)-1]
--- Training.py
+++ Training.py
@@ -1,34 +1,53 @@
-def factorial (number) :
+def inside_out(st):
+    
+    alist = list([x for x in st.split(" ")]) if len(st) > 1 else [st]
+    string = ""
 
-    if number == 1 or number <= 0:
-        return 1
-    else:
-        return number * factorial (number-1)
-
-def make_combination (n, r):
-
-    result = int(factorial(n) / (factorial(n-r) * factorial(r)))
-    return result
-
-def get_participants(handshakes):
-
-    if handshakes == 0:
-        return 1
-    elif handshakes == 1:
-        return 2
-    elif handshakes == 2:
-        return 3
-    else:
-
-        counter = 3
-        while (True):
-
-            attemp = make_combination(counter, 2)
-            if attemp >= handshakes:
-                break
-            else:
-                counter += 1
+    for word in alist:
         
-        return counter
+        result = ""
 
-print(get_participants(6))
+        if len(word) % 2 == 0:
+
+            left_characters = [word[x] for x in range(0, int(len(word) / 2))]
+            right_characters = [word[y] for y in range(int(len(word) / 2), len(word))]
+
+            for char in left_characters[::-1]:
+                result += char
+            for char in right_characters[::-1]:
+                result += char
+
+        else:
+            
+            if len(word) > 1:
+
+                lose_character = word[int(len(word) / 2)]
+                left_characters = [word[x] for x in range(0, int(len(word) / 2))]
+                right_characters = [word[y] for y in range(int(len(word) / 2) + 1, len(word) )]
+
+                for char in left_characters[::-1]:
+                    result += char
+                result += lose_character
+                for char in right_characters[::-1]:
+                    result += char
+            
+            else:
+                result = word
+
+        string += result + " "
+
+    if st[0] == " " and st[len(st)-1] == " ":
+        string = " " + string
+        
+    elif st[0] != " " and st[len(st)-1] == " ":
+        string = " " + string
+
+    elif st[0] == " " and st[len(st)-1] != " ":
+        string = " " + string[::len(str)-2]
+
+    else:
+        continue
+
+    return string
+
+print(inside_out('what time are we climbing up the volcano'))
