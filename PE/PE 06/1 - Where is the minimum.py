61425426f4839d63dfce11634b500de5bf612d62
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-12 12:07:11 +0100

    PE 06 1 Solution

--- PE/PE 06/1 - Where is the minimum.py
+++ PE/PE 06/1 - Where is the minimum.py
@@ -1,2 +1,18 @@
 # Created on April, 2021
 # @author: Fábio Araújo de Sá
+
+def find_min(numbers):
+
+    index = 0
+    min_number = 10000000000000000
+
+    for i in range(len(numbers)):
+        if numbers[i] < min_number:
+            min_number = numbers[i]
+            index = i
+
+    return index
+
+attemps = [[10, 1, 2, 10], [1, 2, 10, 1], [3, 3, -2, 1], [3]]
+for attemp in attemps:
+    print(find_min(attemp))
