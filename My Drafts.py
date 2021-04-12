5ea758e020f3319aad7e8bed2cf30a948e8e2e8c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-12 13:36:04 +0100

    Add PE06 to drafts

--- My Drafts.py
+++ My Drafts.py
@@ -11533,3 +11533,45 @@ attemps = [[1, 4, 3], [1, 2, 10, 1], [5, 0, 7, 0, 2, 2, 1, 1, 5, 1], [3]]
 for attemp in attemps:
     print(diff(attemp))
 
+from math import pow
+
+def decimal_to_binary (number):
+
+    string = ""
+    while (number):
+        aux = number % 2
+        number = (number - aux) // 2
+        string = str(aux) + string
+
+    return int(string) 
+
+def binary_to_decimal (number):
+
+    total = 0
+    power = 0
+    while (number):
+
+        last_digit = number % 10
+        total = total + last_digit * pow(2, power)
+        power += 1
+        number = number // 10
+
+    return int(total)
+
+def binary_range(start, end):
+
+    solution = [start]
+    min_number = binary_to_decimal(start) + 1
+    max_number = binary_to_decimal(end)
+
+    for number in range (min_number, max_number):
+        solution.append(decimal_to_binary(number))
+    
+    for number in solution:
+        yield number
+
+attemps = [[0, 101], [10, 111], [101, 1010], [10001, 11000]]
+for attemp in attemps:
+    start = attemp[0]
+    end = attemp[1]
+    print(binary_range(start, end))
