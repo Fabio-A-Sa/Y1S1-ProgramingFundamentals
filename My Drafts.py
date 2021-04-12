592b2d4e1b873ccafc953fb1707dbb91c74debf4
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-04-12 13:34:52 +0100

    Add PE06 to drafts

--- My Drafts.py
+++ My Drafts.py
@@ -11502,3 +11502,19 @@ def swap(s,n):
     return solution
 
 print(swap('the quick broWn fox leapt over the fence', 11))
+
+def find_min(numbers):
+
+    index = 0
+    min_number = 10000000000000000
+
+    for i in range(len(numbers)):
+        if numbers[i] < min_number:
+            min_number = numbers[i]
+            index = i
+
+    return index
+
+attemps = [[10, 1, 2, 10], [1, 2, 10, 1], [3, 3, -2, 1], [3]]
+for attemp in attemps:
+    print(find_min(attemp))
